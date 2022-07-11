f644903a4c7ae8c7b3baa9e40122d56cfc80f8ef
Kell22-mkt <kellyckaroline@gmail.com>
2022-07-10 21:13:55 -0300

    funçao

---
+++ script.js
@@ -0,0 +1,67 @@
+
+//1 - crie uma função que exiba uma mensagem no console
+function ola(){
+    console.log('ola')
+  }
+  
+  ola()
+//2 - crie uma função que receba o seu nome como (parâmetro) e exiba no console
+function meuNome(nome){
+    console.log(`meu nome é ${nome}`)
+  }
+  meuNome('kelly')
+//3 - crie uma função que receba nome, idade, e um estilo musical preferido (parâmetros) e exiba no console
+function dados(nome, idade, estiloMusical){
+    console.log(`meu nome é ${nome}, eu tenho ${idade} anos e gosto de ${estiloMusical}`)
+  }
+  dados('kelly', 23, 'kizomba')
+//4 - crie uma função que receba um filme, uma música (parâmetros) e exiba no console
+function info(filme, musica){
+    console.log(`eu gosto de ver ${filme} e de escutar ${musica}`)
+  }
+  info('Rock', 'Joji')
+
+//Bônus 
+
+//5 - crie uma função que retorne o triplo do número recebido no parâmetro da função
+function conta(x){
+    return x*3
+  }
+  
+  console.log(conta(5))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
